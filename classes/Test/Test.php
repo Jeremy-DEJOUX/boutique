4839671c7e0aa0ad90cc4f9cf1c80c858940dcb5
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-02-25 09:53:22 +0100

    Test autoloader

---
+++ classes/Test/Test.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Test;
+
+class Test{
+    public function test()
+    {
+        echo "l'autoloader fonctionne";
+    }
+}
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 use App\Autoloader;
+
+use App\Test\Test;
+
 require_once 'classes/Autoloader.php';
 Autoloader::register();
+
+$new = new Test;
+$new->test();
