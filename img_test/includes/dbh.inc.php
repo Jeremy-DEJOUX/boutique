173144461514098e91dd5be2198bc89b733e6356
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-03-23 18:07:07 +0100

    img produit done

---
+++ img_test/includes/dbh.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$conn = new PDO('mysql:host=localhost; dbname=galleryexample; charset=utf8', 'root', '' );
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
