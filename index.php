c277e81e364425e14de595a494aaf23382205ac0
thomas-dellacase <thomas.dellacase@laplatefrome.io>
2021-04-07 15:03:49 +0200

    css index start

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $produits = new Product;
 $path_index="";
 $path_connexion="pages/insCo.php";
 $path_panier="pages/panier.php";
-$path_prouduits="pages/planning.php";
+$path_produits="pages/produits.php";
 $path_admin="pages/reservation.php";
 $path_footer='../css/footer.css';
 
@@ -28,17 +28,28 @@ $path_footer='../css/footer.css';
     <title>Document</title>
 </head>
 <body>
-    <main>
-    </section>
-    <article class="box-slide">
-        <section class="slide-show">
-           <?php $products = $db->query("SELECT * FROM produits"); ?>
-           <?php foreach ($products as $product): ?>
-            
-                   <a class="a-slide" href="pages/produit.php?id=<?= $product->id; ?>"><img class='img-slide'src="ressources/img/<?=$product->FullNameImg ?>" alt="Img Produits"></a>
+    <?php require_once('pages/header.php'); ?>
+        <main>
 
-           <?php endforeach; ?>
-        </section>
-    </main>
+        <article class="box-slide">
+            <h1 id="titre-prod">Nos Derniers produits</h1>
+            <section class="slide-show">
+            <?php $products = $db->query("SELECT * FROM produits"); ?>
+            <?php foreach ($products as $product): ?>
+                
+                    <a class="a-slide" href="pages/produit.php?id=<?= $product->id; ?>"><img class='img-slide'src="ressources/img/<?=$product->FullNameImg ?>" alt="Img Produits"></a>
+
+            <?php endforeach; ?>
+            </section>
+        </article>
+        <article id='art-index'>
+                <section id='sec-index'>
+                <h1>A propos de nous</h1>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed mollitia itaque autem repellendus, animi nesciunt dolores omnis ipsa quasi? Id vitae adipisci esse temporibus et nobis maxime non quae? Nesciunt.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto maxime, perspiciatis possimus id dolorum doloremque aliquid ratione saepe repellendus vitae! Dolorum ex dolore itaque fugiat molestiae ut doloremque voluptate esse!
+                </section>
+           
+        </main>
+        <?php require_once('pages/footer.php');?>
 </body>
 </html>
