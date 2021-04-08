8e659eff4b5d87541c2cbafc61866ca0b5b0f733
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-04-08 14:27:30 +0200

    debut page profil

--- pages/panier.php
+++ pages/panier.php
@@ -26,6 +26,8 @@ if (isset($_POST['confirmCommande'])) {
     <link rel="stylesheet" href="../ressources/CSS/Panier.css">
 </head>
 <body>
+
+    <?php if (!empty($_SESSION['panier'])): ?>
     
     <main>
         
@@ -89,34 +91,23 @@ if (isset($_POST['confirmCommande'])) {
         </div>
     </div>
 
-
-
-
-            
-
-
-            <!-- <form method="post">
-                <p>Nom <?= $product->nom; ?> <br/></p>
-                <p>Prix <?= number_format($product->prix, 2); ?></p> <br/>
-                <img src="../ressources/img/<?= $product->FullNameImg; ?>" alt=""> <br>
-                <span>Quantité <input type="text" name="panier[quantity][<?= $product->id; ?>]" value="<?= $_SESSION['panier'][$product->id]; ?>"></span>
-                <a href="panier.php?delPanier=<?= $product->id;?>">Suprimmer</a> -->
-                <?php //endforeach; ?>
-
-                <!-- <input type="submit" value="Recalculer">
-            </form>
-
-            <p>Somme des produits <?= $panier->count(); ?> </p>
-
-            <p>Prix total du panier <?= number_format($panier->total(), 2); ?></p>
             
             <form action="" method="post">
                 <button name="confirmCommande">Confirmez la Commande</button>
-            </form> -->
+            </form>
 
 
 
     </main>
+
+    <?php else: ?>
+
+    <main>
+        <h1>Votre Panier est Vide</h1>
+        <p>Remplisser le <a href="produits.php">ici</a></p>
+    </main>
+
+    <?php endif; ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="../ressources/JS/script.js"></script>
 </body>
