18f51614ae0564d987a270efd96b1874e4296326
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-04-08 14:34:31 +0200

    Accept My change

--- pages/panier.php
+++ pages/panier.php
@@ -114,11 +114,3 @@ if (isset($_POST['confirmCommande'])) {
 </html>
 
 
-<<<<<<< HEAD
-=======
-</main>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script type="text/javascript" src="../ressources/JS/script.js"></script>
-</body>
-</html>
->>>>>>> thomas
